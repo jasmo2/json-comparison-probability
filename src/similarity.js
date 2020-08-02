6517c48fb2da5afb0680430f163612b0f6409c3e
jaime <jaime.molina@mattersupply.co>
2020-08-02 12:29:35 -0500

    refactor, take in account 'null' && 'undefined' values
    Basic

--- src/similarity.js
+++ src/similarity.js
@@ -5,6 +5,10 @@
 * @returns sorted object by "key"
 */
 function sortObj(obj) {
+  if (obj === null || obj === undefined) {
+    return obj
+  }
+  
   const objKeys = Object.keys(obj)
   const sortedObj = objKeys.sort().reduce((sortedObj, key) => {
     const element = obj[key]
@@ -23,50 +27,53 @@ function sortObj(obj) {
     
     return sortedObj
   },{})
+  
+  return sortedObj
+}
 
-    return sortedObj
+/**
+* @func areObjectsDifferent is the objects 
+* are completly different
+*/
+function areObjectsDifferent(objA, objB) {
+  if(
+    typeof objA !== typeof objB 
+    || Array.isArray(objA) !== Array.isArray(objB)
+    ) {
+      return true
+    }
   }
-  /**
-  * @func areObjectsDifferent is the objects 
-  * are completly different
-  */
-  function areObjectsDifferent(objA, objB) {
-    if(
-      typeof objA !== typeof objB 
-      || Array.isArray(objA) !== Array.isArray(objB)
-      ) {
-        return true
-      }
-    }
-    
-    /**
-    * @func areObjectsEqual is the objects 
-    * are equal to break the cycle
-    */
-    function areObjectsEqual(objA, objB) {
-      const sortedStrA = JSON.stringify(sortObj(objA))
-      const sortedStrB = JSON.stringify(sortObj(objB))
-      
-      // compare strings to know if they are the same.
-      return sortedStrA === sortedStrB
-    }
-    
-    /**
-    * @param { object, string, array} objA 
-    * @param { object, string, array} objB
-    * @returns 0 >= score and 1<=score
-    */
-    function getSimilarity(objA, objB) {
-      
-      if (areObjectsDifferent(objA, objB)) {
-        //The objects comparing each other are completly diferent
-        return 0
-      }
-      
-      if (areObjectsEqual(objA, objB)) {
-        //The objects comparing each other are completly equal
-        return 1
-      }
-    }
-    
-    module.exports = getSimilarity
+  
+/**
+* @func areObjectsEqual is the objects 
+* are equal to break the cycle
+*/
+function areObjectsEqual(objA, objB) {
+  const sortedStrA = JSON.stringify(sortObj(objA))
+  const sortedStrB = JSON.stringify(sortObj(objB))
+  
+  // compare strings to know if they are the same.
+  return sortedStrA === sortedStrB
+}
+
+/**
+* @param { object, string, array} objA 
+* @param { object, string, array} objB
+* @returns 0 >= score and 1<=score
+*/
+function getSimilarity(objA, objB) {
+  
+  if (areObjectsDifferent(objA, objB)) {
+    //The objects comparing each other are completly diferent
+    return 0
+  }
+  
+  if (areObjectsEqual(objA, objB)) {
+    //The objects comparing each other are completly equal
+    return 1
+  }
+  
+  const percentage = computeSimilarity(objA, objB)
+}
+  
+module.exports = getSimilarity
